11577fddae1e4d8ccb8b6704835f6108d1357a88
Celina Louissaint <celinalouissaint@gmail.com>
2023-06-30 15:49:11 -0700

    Install MUI and build layout skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import * as React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
---
+++ utils/mockData.tsx
@@ -0,0 +1,2 @@
+export const mockMediaData =
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" };
